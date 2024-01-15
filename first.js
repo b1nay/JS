78dea4dfcc968fa2322553b7c1c1ec2c6a9dbde9
smrn001 <samir.niroula001@gmail.com>
2024-01-15 18:14:16 +0545

    1st

---
+++ first.js
@@ -0,0 +1,7 @@
+console.log("Console ma print vayo hai ta ");
+console.log("Aaru ni print hudai xa ");
+let name = "Samir Niroula";
+let age = 19;
+console.log("Your name is " + name);
+console.log("Your age is " + age);
+console.log("Your age after 1 year will be " + ++age);
