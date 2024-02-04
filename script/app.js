b22bb454235f43e4081350cf16190d11661eacc0
b1nay <b1nay@icloud.com>
2024-02-04 21:37:17 +0545

    Dark Mode

--- script/app.js
+++ script/app.js
@@ -16,3 +16,62 @@ leapCheck = () => {
     alert(year + " is Invalid Year");
   }
 };
+
+
+//Theme Toggle
+
+const sunIcon = document.querySelector(".sun");
+const moonIcon = document.querySelector(".moon");
+const userTheme = localStorage.getItem("theme");
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+// const iconToggleMoon = () => {
+//   moonIcon.classList.toggle("diplay-none");
+//   sunIcon.classList.toggle("display-block");
+// };
+// const iconToggleSun = () => {
+//   moonIcon.classList.toggle("diplay-block");
+//   sunIcon.classList.toggle("display-none");
+// };
+
+const iconToggle = () => {
+  moonIcon.classList.toggle("diplay-none");
+  sunIcon.classList.toggle("display-none");
+};
+
+const themeCheck = () => {
+  if (userTheme == "dark" || (!userTheme && systemTheme)) {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "light");
+    moonIcon.classList.add("display-none");
+
+    return;
+  }
+  sunIcon.classList.add("display-none");
+};
+
+const themeSwitch = () => {
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+    moonIcon.classList.toggle("display-none");
+    sunIcon.classList.toggle("display-none");
+
+    return;
+  }
+
+  document.documentElement.classList.add("dark");
+  localStorage.setItem("theme", "dark");
+  moonIcon.classList.toggle("display-none");
+  sunIcon.classList.toggle("display-none");
+  // iconToggle();
+};
+
+sunIcon.addEventListener("click", () => {
+  themeSwitch();
+});
+
+moonIcon.addEventListener("click", () => {
+  themeSwitch();
+});
+themeCheck();
